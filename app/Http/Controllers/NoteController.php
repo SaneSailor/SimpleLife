861458f3054074a620e3c0a8dc00c6f3b07d33cd
Tamunowari93 <tamunowari93@gmail.com>
2023-04-28 00:04:38 -0400

    Notes MVM Essentials

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,7 @@ class NoteController extends Controller
      */
     public function index()
     {
-        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
+        $notes = Note::where('user_id', Auth::id())->latest('updated_at')->paginate(3);
         return view('notes.index')->with('notes', $notes);        
     }
 
@@ -22,7 +23,7 @@ class NoteController extends Controller
      */
     public function create()
     {
-        //
+        return view('notes.create');
     }
 
     /**
@@ -30,38 +31,75 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required'
+        ]);
+
+        Note::create([
+            'uuid' => Str::uuid(),
+            'user_id' => Auth::id(),
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+        return to_route('notes.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+        return view('notes.show')->with('note', $note);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+        return view('notes.edit')->with('note', $note);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required'
+        ]);
+
+        $note->update([
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+
+        return to_route('notes.update', $note)->with('success','Note updated successfully');
+        
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Note $note)
     {
-        //
+        if($note->user_id != Auth::id()){
+            return abort(403);
+        }
+
+        $note->delete();
+        return to_route('notes.index')->with('success', 'Note deleted successfully');
     }
 }
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -7,10 +7,17 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <x-alert-success>
+                {{ session('success') }}
+            </x-alert-success>
+
+            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+            
             @forelse ($notes as $note)
                 <div class="my-6 p-6 bg-white border-gray-200 shadow-sm sm:rounded-lg">
                     <h2 class="font-bold text-2xl">
-                        {{ $note->title }}
+                        <a href="{{ route('notes.show', $note) }}">{{ $note->title }}</a>
                     </h2>
                     <p class="mt-2">
                         {{ Str::limit($note->text, 200) }}
